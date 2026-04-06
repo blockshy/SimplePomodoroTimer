19178e806b22b7383dcc312762999b1c59e7afa0
blockshy <2395205443@qq.com>
2026-04-06 14:31:29 +0800

    Add application deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,6 @@ export default defineConfig(({mode}) => {
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
+    base: './', // 关键配置：设置部署的子路径，与 Nginx 的 location 对应
   };
 });
